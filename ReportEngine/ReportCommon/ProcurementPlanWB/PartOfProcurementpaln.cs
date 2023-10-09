01be4b35ecb9c24c74ea735f946d696a75e209ff
Mekontso <modeste.mekontso@outlook.com>
2023-10-09 11:16:20 -0400

    procurement plan WB

---
+++ ReportEngine/ReportCommon/ProcurementPlanWB/PartOfProcurementpaln.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportCommon.ProcurementPlanWB
+{
+    public class PartOfProcurementplan
+    {
+        public ProjectInformations projectinformation { get; set; }
+        public string BanksapprovaldateofProcurementplan { get; set; }
+        public string DateofGeneralProcurementNotice { get; set; }
+        public DateTime Periodcoveredbythisprocurementplan { get; set; }
+        public WorksandGoodsandNonConsultantServices worksandGoodsandNonConsultantServices { get; set; }
+        public SelectionofConsultant selectionofConsultants { get; set; }
+    }
+}
