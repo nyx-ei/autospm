0eb47bd01acea3296d9de016e46da9aabda20a2f
GT_Codeur <tegomo2lu@gmail.com>
2024-11-06 15:16:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ from secrets import token_hex
 from fastapi_mail import ConnectionConfig
 from dotenv import dotenv_values
 import os
-SECRET_TOKEN = token_hex(25)
-TOKEN_ALGORITHM = 'HS256'
 
 config_credentials = dotenv_values(os.environ["ENV_PATH"])
+
+SECRET_TOKEN = token_hex(25)
+TOKEN_ALGORITHM = config_credentials["TOKEN_ALGORITHM"]
+
 conf = ConnectionConfig(
     MAIL_USERNAME=config_credentials['MAIL'],
     MAIL_PASSWORD=config_credentials["PASSWORD"],
