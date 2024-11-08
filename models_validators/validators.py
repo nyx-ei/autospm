6f100aa269e61df655a083b60e1e2080680349cb
GT_Codeur <tegomo2lu@gmail.com>
2024-11-08 02:06:54 +0100

    Update validators.py
    Fixing validators

--- models_validators/validators.py
+++ models_validators/validators.py
@@ -1,42 +1,31 @@
 import re
-from pydantic import BaseModel, EmailStr
-from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, Field, field_validator
 from datetime import date, datetime
 
 
 class UserValidation(BaseModel):
     username: str
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8)
     name: str
     firstname: str
     date_of_birth: date
     phone_number: str
     address: str
 
-    def check_valid_pwd(self):
-        pwd = self.password
-        if len(pwd) < 8:
-            raise HTTPException(
-                status_code=403,
-                detail="Password must contain at least 8 characters."
-            )
-        if not re.search(r'[A-Z]', pwd):
-            raise HTTPException(status_code=403, detail="Password must contain at least one uppercase letter.")
-        if not re.search(r'[a-z]', pwd):
-            raise HTTPException(status_code=403, detail="Password must contain at least one lowercase letter.")
-        if not re.search(r'[0-9]', pwd):
-            raise HTTPException(status_code=403, detail="Password must contain at least one digit.")
-        if not re.search(r'[\W_]', pwd):
-            raise HTTPException(status_code=403, detail="Password must contain at least one special character.")
-        return pwd
+    @field_validator('password')
+    def check_valid_pwd(cls, v):
+        if not re.match(r"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).*$", v):
+            raise ValueError("Password must contain at least one uppercase letter, one lowercase letter"
+                             "one digit and at least on special character.")
+        return v
 
-    def check_valid_phone_number(self):
-        if not re.match(r"\+2376[25789][0-9]{7}$", self.phone_number):
-            raise HTTPException(status_code=403,
-                                detail="Phone number should be in in this form: +2376 followed by 2 "
-                                       "or 5 or 7 or 8 or 9 and with any 7 digits"
-                                )
+    
+    @field_validator('phone_number')
+    def check_valid_phone_number(cls, v):
+        if not re.match(r"\+2376[25789][0-9]{7}$", v):
+            raise ValueError("Phone number must be in the form +237 follows by 9 digits")
+        return v.title()
 
 
 class Captcha(BaseModel):
