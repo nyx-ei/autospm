8781c66ae756e24f8a86b002689362772844ed9c
GT_Codeur <tegomo2lu@gmail.com>
2024-11-08 01:59:28 +0100

    Update main.py
    Fixing validation while creating a user account

--- main.py
+++ main.py
@@ -27,43 +27,44 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-# Route to register a user.
-@app.post('/registration')
-async def create_user(user: UserValidation, db: db_dependency):
-    # Check if the user exist
+# This function checks if the user exist
+async def user_exit(user: UserValidation, db: db_dependency) -> bool:
     if db.scalar(select(User).where(User.username == user.username)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"User with username: {user.username} already exists."
         )
+    return True
 
-    # Validate password and phone number fields
-    user.check_valid_pwd()
-    user.check_valid_phone_number()
 
-    # Creation of an unverified user in the database
-    user_obj = User(
-        username=user.username,
-        email=user.email,
-        password=get_hashed_password(user.password),
-        name=user.name,
-        firstname=user.firstname,
-        date_of_birth=user.date_of_birth,
-        phone_number=user.phone_number,
-        address=user.address,
-        is_verified=False
-    )
-    db.add(user_obj)
-    db.commit()
-
-    # Send confirmation email
-    await send_email([user_obj.email], user_obj)
-
-    return {
-        "status": "Ok",
-        "data": f"Hello {user_obj.username}, thanks for choosing our services. Please check your email inbox and "
-                f"click on the link to confirm your email"
-    }
+# Route to register a user.
+@app.post('/registration')
+async def create_user(user: UserValidation, db: db_dependency):
+    # Check if the user exist
+    if await user_exit(user, db):
+        # Creation of an unverified user in the database
+        user_obj = User(
+            username=user.username,
+            email=user.email,
+            password=get_hashed_password(user.password),
+            name=user.name,
+            firstname=user.firstname,
+            date_of_birth=user.date_of_birth,
+            phone_number=user.phone_number,
+            address=user.address,
+            is_verified=False
+        )
+        db.add(user_obj)
+        db.commit()
+    
+        # Send confirmation email
+        await send_email([user_obj.email], user_obj)
+    
+        return {
+            "status": "Ok",
+            "data": f"Hello {user_obj.username}, thanks for choosing our services. Please check your email inbox and "
+                    f"click on the link to confirm your email"
+        }
 
 
 template = Jinja2Templates(directory="templates")
